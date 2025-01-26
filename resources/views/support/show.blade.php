5f2ee8405aeb32c6466163cec80f18954e37d1d5
Erfan Farokhi <erfanfarokhi84@gmail.com>
2025-01-26 16:04:58 +0330

    change some styles in ticket page

--- resources/views/support/show.blade.php
+++ resources/views/support/show.blade.php
@@ -67,7 +67,7 @@
             <div id="accordion-open-2-body-1" class="hidden" aria-labelledby="accordion-open-2-heading-1">
                 <div class="border border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                     <ul class="text-sm font-medium text-gray-900 bg-white dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                        <a href="#">
+                        <a href="{{ route('support.index') }}">
                             <li class="w-full flex items-center gap-1 px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-right-fill" viewBox="0 0 16 16">
                                     <path d="M14 0a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
@@ -80,12 +80,16 @@
             </div>
         </div>
 
-        <a href="#" class="flex items-center justify-center gap-1 col-span-2 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-            </svg>
-            بستن درخواست
-        </a>
+        <form class="col-span-2" method="POST" action="{{ route('support.close-ticket', $ticket) }}">
+            @csrf
+            <button @if($ticket->status) disabled @endif type="submit" class="flex items-center justify-center gap-1 focus:outline-none @if($ticket->status) bg-red-300 cursor-not-allowed @else bg-red-700 hover:bg-red-800 @endif text-white focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                </svg>
+                بستن درخواست
+            </button>
+        </form>
+
         <a href="#" class="flex items-center justify-center gap-1 col-span-2 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
@@ -101,7 +105,17 @@
             </div>
         </div>
         <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
-
+        <div class="flex flex-col justify-between gap-3">
+            <div class="flex flex-col gap-1">
+                <span class="font-bold text-l">عناون: </span>
+                <div>{{ $ticket->title }}</div>
+            </div>
+            <div class="flex flex-col gap-1">
+                <span class="font-bold">توضیحات: </span>
+                <div class="text-gray-500">{{ $ticket->description }}</div>
+            </div>
+        </div>
+        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
     </div>
 </div>
 @endsection
