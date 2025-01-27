913d61f66e250e966ddf80471c2dffa9690f7805
Erfan Farokhi <erfanfarokhi84@gmail.com>
2025-01-27 10:49:40 +0330

    fix a bug in profile section

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -90,7 +90,4 @@
             </div>
         </div>
     @endif
-
-    <script src="{{ asset('js/new-meeting-people.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
 @endsection
